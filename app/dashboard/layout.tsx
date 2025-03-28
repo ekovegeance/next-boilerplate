dbe0eb2558bca6160a0f98da62a869a1de1058a7
Eko <ekovegeance7@gmail.com>
2025-03-29 01:12:17 +0700

    Update README, enhance session management in dashboard layout, and modify register form for password input; upgrade next-auth version

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,7 +1,11 @@
 import AppLayoutTemplate from "@/layouts/app/app-header-layout";
 import { type ReactNode } from "react";
-
+import { SessionProvider } from "next-auth/react";
 
 export default function AppLayout({ children }: { children: ReactNode }) {
-  return <AppLayoutTemplate>{children}</AppLayoutTemplate>;
+  return (
+    <SessionProvider>
+      <AppLayoutTemplate>{children}</AppLayoutTemplate>
+    </SessionProvider>
+  );
 }
