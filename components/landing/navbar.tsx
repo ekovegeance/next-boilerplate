f62fca1226838681b17c018a382b68801d2b2f38
Eko <ekovegeance7@gmail.com>
2025-04-04 17:42:59 +0700

    Update navbar styles for improved accessibility and visual consistency

--- components/landing/navbar.tsx
+++ components/landing/navbar.tsx
@@ -38,7 +38,7 @@ export function Navbar() {
   const getInitials = useInitials();
 
   return (
-    <nav className="border-border/40 fixed top-0 right-0 left-0 z-50 border-b bg-background backdrop-blur">
+    <nav className="border-border/40 bg-background/95 supports-[backdrop-filter]:bg-background/60 sticky top-0 right-0 left-0 z-50 border-b backdrop-blur">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16">
           <div className="flex">
@@ -49,22 +49,22 @@ export function Navbar() {
                 </div>
               </Link>
             </div>
-            {/* <div className="hidden sm:ml-6 sm:flex sm:space-x-8">
-              {navLinks.map((link) => (
+            <div className="hidden sm:ml-6 sm:flex sm:space-x-8">
+              {/* {navLinks.map((link) => (
                 <Link
                   key={link.href}
                   href={link.href}
                   className={clsx(
-                    "inline-flex items-center px-1 pt-1 text-sm font-medium text-zinc-500",
+                    "inline-flex items-center px-1 pt-1 text-sm font-medium",
                     pathname === link.href
-                      ? " text-zinc-900"
-                      : " text-zinc-500 hover:text-zinc-900"
+                      ? " text-primary"
+                      : " text-zinc-500 hover:text-primary"
                   )}
                 >
                   {link.name}
                 </Link>
-              ))}
-            </div> */}
+              ))} */}
+            </div>
           </div>
           <div className="hidden sm:ml-6 sm:flex sm:items-center">
             <Suspense fallback={<NavUserSkeleton />}>
@@ -136,8 +136,8 @@ export function Navbar() {
                 className={clsx(
                   "block pl-3 pr-4 py-2 text-base font-medium",
                   pathname === link.href
-                    ? " text-zinc-900"
-                    : " text-zinc-500 hover:text-zinc-900"
+                    ? " text-primary"
+                    : " text-zinc-500 hover:text-primary"
                 )}
               >
                 {link.name}
